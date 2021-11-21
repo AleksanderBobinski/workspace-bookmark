899d10adf0df647ee3e5e3d77d2e44cf05e99cc7
Aleksander Bobiński <aleksanderbobinski@interia.eu>
2021-11-21 12:22:30 +0100

    Add command line completion

--- setup.sh
+++ setup.sh
@@ -13,3 +13,16 @@ g () {
 	fi
 	return $e;
 }
+
+# https://askubuntu.com/questions/68175/how-to-create-script-with-auto-complete
+_g()
+{
+  possible_destinations=$(echo "$WORKSPACE_BOOKMARKS" | jq --sort-keys 'keys' | sed -E 's/\[|\]/ /g' | tr '"' ' ' | tr ',' ' ')
+
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  COMPREPLY=( $(compgen -W "${possible_destinations}" -- ${cur}) )
+
+  return 0
+}
+complete -o nospace -F _g g
