bd5311e9f1467cd4197c73ce7ee161c7c1ec930e
Aleksander Bobinski <aleksander.bobinski@interia.eu>
2023-01-08 13:13:13 +0100

    src/conftest.py: Create magic workspace fixture
    The .repo workspace used as the only workspace for testing is now
    created under a magic workspace. It will be now possible to test
    previously untested use-cases using the new fixture "magic_workspace".
    This includes improving "test_use_magic_file_instead_of_repo" which
    succeeds in proving the implementation works as expected but don't
    actually test the use-case it was supposed to test.
    
    The fixture "workspace" is renamed to "repo_workspace" to better describe
    it's purpose.
    
    The "magic_filename" is a fixture because it will be used for two
    purposes:
      1. Provide name of magic directory when construing magic workspace
      2. Populate WORKSPACE_BOOKMARK_MAGIC_FILE
    Those are to be done in two separate fixtures but need the same randomly
    generated filename.

--- src/conftest.py
+++ src/conftest.py
@@ -8,46 +8,6 @@ import string
 import pytest
 
 
-@pytest.fixture(name="workspace")
-def construct_workspace(tmp_path):
-    """Construct a workspace used for testing."""
-    workspace_root = tmp_path / "ws"
-    workspace_root.mkdir()
-    repo = workspace_root / ".repo"
-    repo.mkdir()
-    android = workspace_root / "android"
-    android.mkdir()
-    poky = workspace_root / "poky"
-    poky.mkdir()
-    build = workspace_root / "poky" / "build"
-    build.mkdir()
-    return workspace_root
-
-
-@pytest.fixture(name="REPO_DIRECTORY")
-def get_workspace_root(workspace):
-    """Return path to a workspace's root directory."""
-    return str(workspace)
-
-
-@pytest.fixture(name="ANDROID_DIRECTORY")
-def get_android_directory(workspace):
-    """Return path to a android directory."""
-    return str(workspace / "android")
-
-
-@pytest.fixture(name="POKY_DIRECTORY")
-def get_poky_directory(workspace):
-    """Return path to a poky directory."""
-    return str(workspace / "poky")
-
-
-@pytest.fixture(name="BUILD_DIRECTORY")
-def get_build_directory(workspace):
-    """Return path to a build directory."""
-    return str(workspace / "poky/build")
-
-
 def get_random_string(alphabet, length):
     """Return a random string of length using a specified alphabet."""
     return ''.join(random.choices(alphabet, k=length))
@@ -63,6 +23,64 @@ def get_random_directory_name():
     return random_directory_name
 
 
+@pytest.fixture(name="magic_filename")
+def get_magic_filename():
+    """Get name used for WORKSPACE_BOOKMARK_MAGIC_FILE but don't set env."""
+    name_length = 20
+    alphabet = string.ascii_letters + string.digits
+    return get_random_string(alphabet, name_length)
+
+
+@pytest.fixture(name="magic_workspace")
+def construct_magic_workspace(tmp_path, magic_filename):
+    """Construct an outer magic workspace used for testing."""
+    magic_workspace_root = tmp_path / get_random_directory_name()
+    magic_workspace_root.mkdir(parents=True)
+    wsmagic = magic_workspace_root / magic_filename
+    wsmagic.mkdir()
+    return magic_workspace_root
+
+
+@pytest.fixture(name="repo_workspace")
+def construct_repo_workspace(magic_workspace):
+    """Construct an inner .repo workspace used for testing."""
+    repo_workspace_root = magic_workspace / get_random_directory_name()
+    repo_workspace_root.mkdir(parents=True)
+    repo = repo_workspace_root / ".repo"
+    repo.mkdir()
+    android = repo_workspace_root / "android"
+    android.mkdir()
+    poky = repo_workspace_root / "poky"
+    poky.mkdir()
+    build = repo_workspace_root / "poky" / "build"
+    build.mkdir()
+    return repo_workspace_root
+
+
+@pytest.fixture(name="REPO_DIRECTORY")
+def get_workspace_root(repo_workspace):
+    """Return path to a workspace's root directory."""
+    return str(repo_workspace)
+
+
+@pytest.fixture(name="ANDROID_DIRECTORY")
+def get_android_directory(repo_workspace):
+    """Return path to a android directory."""
+    return str(repo_workspace / "android")
+
+
+@pytest.fixture(name="POKY_DIRECTORY")
+def get_poky_directory(repo_workspace):
+    """Return path to a poky directory."""
+    return str(repo_workspace / "poky")
+
+
+@pytest.fixture(name="BUILD_DIRECTORY")
+def get_build_directory(repo_workspace):
+    """Return path to a build directory."""
+    return str(repo_workspace / "poky/build")
+
+
 @pytest.fixture(name="_current_location_outside_any_workspace")
 def set_current_location_to_outside_any_workspace(monkeypatch, tmp_path):
     """Return path to random location outside workspace."""
@@ -72,8 +90,8 @@ def set_current_location_to_outside_any_workspace(monkeypatch, tmp_path):
 
 
 @pytest.fixture(name="_current_location_inside_repo_workspace")
-def set_current_location_to_inside_repo_workspace(monkeypatch, workspace):
+def set_current_location_to_inside_repo_workspace(monkeypatch, repo_workspace):
     """Return path to random location inside workspace."""
-    current_location = workspace / get_random_directory_name()
+    current_location = repo_workspace / get_random_directory_name()
     current_location.mkdir(parents=True)
     monkeypatch.chdir(current_location)
