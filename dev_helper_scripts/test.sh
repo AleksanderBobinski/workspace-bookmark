2850670414cf0dc30e5a6a13a101a4b294d4a771
Aleksander Bobinski <aleksander.bobinski@interia.eu>
2023-09-26 15:44:22 +0200

    Add an integration test workflow
    This workflow contains dependencies needed by bitbake to run
    minimalistic builds.

--- dev_helper_scripts/test.sh
+++ dev_helper_scripts/test.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
 act -W .github/workflows/lint.yaml && \
-act -W .github/workflows/test.yaml
-
+act -W .github/workflows/test.yaml && \
+act -W .github/workflows/integration-test.yaml && \
+echo OK!
